bfaaeefc361576ae311100e8a5db55c357dfee81
hudsonchen <hudsonchen33@outlook.com>
2026-01-21 15:00:40 +0000

    add_config

---
+++ scripts4LM/run_more.sh
@@ -0,0 +1,53 @@
+for seed in {2..5}
+do
+  for d in 10
+    do
+      for g in 0 1 2
+      do
+      for particle_num in 64 256 1024
+      do
+      for kt_function in compress_kt
+      do
+      /home/zongchen/miniconda3/envs/thin_mfld/bin/python run_mfld.py --dataset student_teacher --seed $seed --g $g --particle_num $particle_num --step_size 0.01 --noise_scale 0.0 --bandwidth 1.0 --step_num 100 --thinning kt --kernel gaussian --zeta 0.0001 --d $d --teacher_num 100 --kt_function $kt_function --skip_swap
+      done
+      done
+    done
+    done
+done
+
+for seed in {0..5}
+do
+  for thinning in kt
+  do
+    for particle_num in 64 256 1024
+    do
+      for zeta in 0.0
+        do
+        for g in 1 2
+        do
+        for kt_function in compress_kt
+        do
+          /home/zongchen/miniconda3/envs/thin_mfld/bin/python run_mfld.py --seed $seed --dataset mmd_flow --particle_num $particle_num --step_size 1.0 --noise_scale 3e-4 --step_num 15000 --thinning $thinning --kernel gaussian --zeta $zeta --g $g --kt_function $kt_function --skip_swap
+        done
+        done
+        done
+    done
+  done
+done
+
+# for seed in {0..5}
+# do
+#   for kernel in sobolev
+#   do
+#     for g in 0 1 2
+#     do
+#       for particle_num in 4096
+#       do
+#         for kt_function in compress_kt
+#           do
+#             /home/zongchen/miniconda3/envs/thin_mfld/bin/python run_mfld.py --seed $seed --dataset vlm --g $g --particle_num $particle_num --step_size 0.0001 --noise_scale 0.001 --bandwidth 1.0 --step_num 200 --thinning kt --kernel $kernel --zeta 0.1 --kt_function $kt_function --skip_swap
+#           done
+#       done
+#     done
+#   done
+# done
