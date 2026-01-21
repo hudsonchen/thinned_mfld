de20908a1036aba8dca0151abb44c6a2cdb64f8d
hudsonchen <hudsonchen33@outlook.com>
2026-01-21 14:59:07 +0000

    update

--- scripts/local_all.sh
+++ scripts/local_all.sh
@@ -4,7 +4,7 @@ do
     do
       for g in 0 1 2
       do
-      for particle_num in 64 256 1024 4096
+      for particle_num in 64 256 1024
       do
       for kt_function in compress_kt
       do
@@ -19,7 +19,7 @@ for seed in {0..5}
 do
   for thinning in kt
   do
-    for particle_num in 64 256 1024 4096
+    for particle_num in 64 256 1024
     do
       for zeta in 0.0
         do
@@ -35,19 +35,19 @@ do
   done
 done
 
-for seed in {0..5}
-do
-  for kernel in sobolev
-  do
-    for g in 0 1 2
-    do
-      for particle_num in 4096
-      do
-        for kt_function in compress_kt
-          do
-            /home/zongchen/miniconda3/envs/thin_mfld/bin/python run_mfld.py --seed $seed --dataset vlm --g $g --particle_num $particle_num --step_size 0.0001 --noise_scale 0.001 --bandwidth 1.0 --step_num 200 --thinning kt --kernel $kernel --zeta 0.1 --kt_function $kt_function --skip_swap
-          done
-      done
-    done
-  done
-done
+# for seed in {0..5}
+# do
+#   for kernel in sobolev
+#   do
+#     for g in 0 1 2
+#     do
+#       for particle_num in 4096
+#       do
+#         for kt_function in compress_kt
+#           do
+#             /home/zongchen/miniconda3/envs/thin_mfld/bin/python run_mfld.py --seed $seed --dataset vlm --g $g --particle_num $particle_num --step_size 0.0001 --noise_scale 0.001 --bandwidth 1.0 --step_num 200 --thinning kt --kernel $kernel --zeta 0.1 --kt_function $kt_function --skip_swap
+#           done
+#       done
+#     done
+#   done
+# done
