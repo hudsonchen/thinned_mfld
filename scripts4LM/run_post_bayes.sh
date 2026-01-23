06b695c43f274229bce3822729c243f60f474772
Lester Mackey <lmackey@microsoft.com>
2026-01-23 03:37:00 +0000

    adding post bayes results

--- scripts4LM/run_post_bayes.sh
+++ scripts4LM/run_post_bayes.sh
@@ -1,15 +1,15 @@
-for seed in {0..9}
+for seed in {2..9}
 do
-  for method in random rbm
+  for method in random rbm false
   do
     for g in 0
     do
-      for particle_num in 16 64 256 1024 4096
+      for particle_num in 16 64 256 1024
       do
-        /home/zongchen/miniconda3/envs/thin_mfld/bin/python run_mfld.py --seed $seed --dataset vlm --g $g --particle_num $particle_num --step_size 0.0001 --noise_scale 0.001 --bandwidth 1.0 --step_num 150 --thinning $method --kernel sobolev --zeta 0.1
+        taskset -c $((($seed - 2)*12))-$(((($seed - 2)*12)+11)) python run_mfld.py --seed $seed --dataset vlm --g $g --particle_num $particle_num --step_size 0.0001 --noise_scale 0.001 --bandwidth 1.0 --step_num 150 --thinning $method --kernel sobolev --zeta 0.1
       done
     done
-  done
+  done &
 done
 
 
@@ -21,7 +21,7 @@ do
     do
       for particle_num in 16 64 256 1024
       do
-        /home/zongchen/miniconda3/envs/thin_mfld/bin/python run_mfld.py --seed $seed --dataset vlm --g $g --particle_num $particle_num --step_size 0.0001 --noise_scale 0.001 --bandwidth 1.0 --step_num 150 --thinning $method --kernel sobolev --zeta 0.1
+        python run_mfld.py --seed $seed --dataset vlm --g $g --particle_num $particle_num --step_size 0.0001 --noise_scale 0.001 --bandwidth 1.0 --step_num 150 --thinning $method --kernel sobolev --zeta 0.1
       done
     done
   done
